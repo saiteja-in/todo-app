c8b9bcc963d2df977a2f78303c503e0cedaf24f7
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-23 22:50:28 +0530

    two methods to make the input section empty after the add task button click, one is using the useRef hook(to work this you should not put the value={currentList}) since currentlist is a state variable tied to the input's value, you should update currentlist to keep the state in sync with the DOM.  and the second method is just by updating the currentlist="" in the function calling during button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import React,{useState} from 'react'
-import './App.css'
+import React, { useState, useRef } from "react";
+import "./App.css";
 const App = () => {
-
-  const[todolist,setTodolist]=useState([])
-  const[currentlist,setCurrentList]=useState('')
+  const [todolist, setTodolist] = useState([]);
+  const [currentlist, setCurrentList] = useState("");
   // const[display,setDisplay]=useState(false)
+  const inputTask = useRef(null);
 
-  const addTask=()=>{
-    if(currentlist){
-
-      setTodolist([...todolist,currentlist]);
-      setCurrentList('') 
-    }
-  }
+  const addTask = () => {
+    setTodolist([...todolist, currentlist]);
+    inputTask.current.value="";
+  };
   return (
     <>
-    <div className="App">
-      <h1>ToDo Application</h1>
-      <input placeholder="Task.." value={currentlist} onChange={(e)=>setCurrentList(e.target.value)}></input>
-      <button onClick={addTask} >Add Task</button>
-      <hr />
-    </div>
-    <div>
-      <ul>
-        { todolist.map((item,index)=><li key={index}>{item}</li>)}
-      </ul>
-    </div>
+      <div className="App">
+        <h1>ToDo Application</h1>
+        <input
+          ref={inputTask}
+          type="text"
+          placeholder="Task.."
+          // value={currentlist}
+          onChange={(e) => setCurrentList(e.target.value)}
+        ></input>
+        <button onClick={addTask}>Add Task</button>
+        <hr />
+      </div>
+      <div>
+        <ul>
+          {todolist.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
