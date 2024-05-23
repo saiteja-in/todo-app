2a0f24f1775007613e9e9f453551653fea0cfdb9
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-23 21:10:30 +0530

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className="App">
+      <h1>ToDo Application</h1>
+      <input placeholer="Task.."></input>
+      <button>Add Task</button>
+      <hr />
+    </div>
+  )
+}
+
+export default App
