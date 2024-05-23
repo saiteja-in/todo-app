6b66a9fcd7a86c2889a221a64383de9a7ab45139
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-23 22:33:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './App.css'
 const App = () => {
+
+  const[todolist,setTodolist]=useState([])
+  const[currentlist,setCurrentList]=useState('')
+  // const[display,setDisplay]=useState(false)
+
+  const addTask=()=>{
+    if(currentlist){
+
+      setTodolist([...todolist,currentlist]);
+      setCurrentList('') 
+    }
+  }
   return (
+    <>
     <div className="App">
       <h1>ToDo Application</h1>
-      <input placeholer="Task.."></input>
-      <button>Add Task</button>
+      <input placeholder="Task.." value={currentlist} onChange={(e)=>setCurrentList(e.target.value)}></input>
+      <button onClick={addTask} >Add Task</button>
       <hr />
     </div>
+    <div>
+      <ul>
+        { todolist.map((item,index)=><li key={index}>{item}</li>)}
+      </ul>
+    </div>
+    </>
   )
 }
 
